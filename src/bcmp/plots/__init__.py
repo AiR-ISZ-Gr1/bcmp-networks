80b743b3ed7c66d350b92ac5eee100c5420c4c6a
Kacper Jarzyna <kjarzyn001@gmail.com>
2024-11-13 16:07:01 +0100

    u mnie działa

--- src/bcmp/plots/__init__.py
+++ src/bcmp/plots/__init__.py
@@ -1,5 +1,4 @@
-from bcmp.simulation import simulate
-from bcmp.plots import avg_requests_per_class, procces_time_all_servers
+from .plots import avg_requests_per_class, procces_time_all_servers
 
 # Example usage:
 # simulate("configs/networks/example2.json", duration=60)
