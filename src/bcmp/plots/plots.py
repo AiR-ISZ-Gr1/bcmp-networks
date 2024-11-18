0affa4216c3ffa9ebe7bb80d2176758ce9cf3fb6
BartoszBartt <bartosz.bartoszewski21@gmail.com>
2024-11-18 23:30:02 +0100

    change

--- src/bcmp/plots/plots.py
+++ src/bcmp/plots/plots.py
@@ -36,70 +36,70 @@ def plot_avg_time_request_server_with_type(data_subset, server_name):
     if not data_subset['ts'].isna().all():
         data_subset['ts'] = pd.to_timedelta(data_subset['ts'])
     # print(data_subset)
-    data_subset = data_subset.sort_values(by=['request_id', 'ts'])
+        data_subset = data_subset.sort_values(by=['request_id', 'ts'])
 
-    processing_intervals = []
-    new_request_id = None
+        processing_intervals = []
+        new_request_id = None
 
-    type_color_mapping = {
-            "Symulant": "green",
-            "Stabilny": "yellow",
-            "Krytyczny": "red"
-    }
+        type_color_mapping = {
+                "Symulant": "green",
+                "Stabilny": "yellow",
+                "Krytyczny": "red"
+        }
 
-    for request_id, group in data_subset.groupby('request_id'):
-        group = group.reset_index(drop=True)
-        last_type = None
-        for i, row in group.iterrows():
-            if row['action'] == 'received' or (last_type and row.get('new_type') != last_type):
-                current_type = row.get('new_type') if row.get('new_type') else 'initial'
-                new_request_id = f"{request_id}_{i}_{current_type}"
-                
-            if row['action'] == 'received':
-                start_time = row['ts']
-                last_type = row['type']
-            
-            elif row['action'] in ['forwarded', 'rejected']:
-                end_time = row['ts']
-                processing_time = (end_time - start_time).total_seconds()
-
-                if processing_time > 0:
-                    processing_intervals.append({
-                        'request_id': new_request_id, 
-                        'processing_time': processing_time, 
-                        'type': last_type
-                    })
+        for request_id, group in data_subset.groupby('request_id'):
+            group = group.reset_index(drop=True)
+            last_type = None
+            for i, row in group.iterrows():
+                if row['action'] == 'received' or (last_type and row.get('new_type') != last_type):
+                    current_type = row.get('new_type') if row.get('new_type') else 'initial'
+                    new_request_id = f"{request_id}_{i}_{current_type}"
                     
-                last_type = row.get('new_type', 'initial')
+                if row['action'] == 'received':
+                    start_time = row['ts']
+                    last_type = row['type']
+                
+                elif row['action'] in ['forwarded', 'rejected']:
+                    end_time = row['ts']
+                    processing_time = (end_time - start_time).total_seconds()
 
-    processing_intervals_df = pd.DataFrame(processing_intervals)
-    
-    if not processing_intervals_df.empty:
-        processing_intervals_df['new_type'] = processing_intervals_df['type'].map(type_color_mapping)
-        # 
-        average_processing_time_df = processing_intervals_df.groupby(['request_id', 'new_type'])['processing_time'].mean().reset_index()
+                    if processing_time > 0:
+                        processing_intervals.append({
+                            'request_id': new_request_id, 
+                            'processing_time': processing_time, 
+                            'type': last_type
+                        })
+                        
+                    last_type = row.get('new_type', 'initial')
+
+        processing_intervals_df = pd.DataFrame(processing_intervals)
         
-        colors = average_processing_time_df['new_type']
-        st.title(f"Average Processing Time for each Request ID with Type ||| {server_name}")
+        if not processing_intervals_df.empty:
+            processing_intervals_df['new_type'] = processing_intervals_df['type'].map(type_color_mapping)
+            # 
+            average_processing_time_df = processing_intervals_df.groupby(['request_id', 'new_type'])['processing_time'].mean().reset_index()
+            
+            colors = average_processing_time_df['new_type']
+            st.title(f"Average Processing Time for each Request ID with Type ||| {server_name}")
 
 
-        fig, ax = plt.subplots(figsize=(10, 6))
-        color = average_processing_time_df['new_type']
-        ax.bar(average_processing_time_df['request_id'], average_processing_time_df['processing_time'], color=color)
-        ax.set_xlabel('Request ID (with Type)')
-        ax.set_ylabel('Average Processing Time (seconds)')
-        ax.set_title(f'Average Processing Time for each Request ID with Type ||| {server_name}')
+            fig, ax = plt.subplots(figsize=(10, 6))
+            color = average_processing_time_df['new_type']
+            ax.bar(average_processing_time_df['request_id'], average_processing_time_df['processing_time'], color=color)
+            ax.set_xlabel('Request ID (with Type)')
+            ax.set_ylabel('Average Processing Time (seconds)')
+            ax.set_title(f'Average Processing Time for each Request ID with Type ||| {server_name}')
 
-        legend_elements = [
-        Line2D([0], [0], color="green", lw=2, label="Symulant"),
-        Line2D([0], [0], color="yellow", lw=2, label="Stabilny"),
-        Line2D([0], [0], color="red", lw=2, label="Krytyczny")
-    ]
-        plt.legend(handles=legend_elements, loc="upper left")
+            legend_elements = [
+            Line2D([0], [0], color="green", lw=2, label="Symulant"),
+            Line2D([0], [0], color="yellow", lw=2, label="Stabilny"),
+            Line2D([0], [0], color="red", lw=2, label="Krytyczny")
+        ]
+            plt.legend(handles=legend_elements, loc="upper left")
 
 
-        ax.set_xticks([])
-        st.pyplot(fig)
-    else:
-        st.write(f"No valid processing times for source subset.")
+            ax.set_xticks([])
+            st.pyplot(fig)
+        else:
+            st.write(f"No valid processing times for source subset.")
     
